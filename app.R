d4242864f68c95ed2c0d248b08a29da2dd8d43ec
Patrick Kelly <phototala@gmail.com>
2019-02-26 12:57:22 -0500

    Add files via upload

---
+++ app.R
@@ -0,0 +1,61 @@
+library(shiny)
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Downloading Data"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Choose dataset ----
+      selectInput("dataset", "Choose a dataset:",
+                  #choices = c("rock", "pressure", "cars")),
+      choices = c("rock")),
+      # Button
+      downloadButton("downloadData", "Download")
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      tableOutput("table")
+      
+    )
+    
+  )
+)
+
+server <- function(input, output) {
+  
+  # Reactive value for selected dataset ----
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = rock)
+           #"pressure" = pressure,
+           #"cars" = cars)
+  })
+  
+  # Table of selected dataset ----
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
+  
+}
+
+shinyApp(ui = ui, server = server)
+
